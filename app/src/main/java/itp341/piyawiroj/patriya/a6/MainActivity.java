549590c0e37ec951955918e6db2df133a8de90f1
Pearl <imnotateapot@gmail.com>
2019-10-04 19:28:00 -0700

    Configured all rooms

--- app/src/main/java/itp341/piyawiroj/patriya/a6/MainActivity.java
+++ app/src/main/java/itp341/piyawiroj/patriya/a6/MainActivity.java
@@ -10,13 +10,36 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button room1Button;
+    private Button room2Button;
+    private Button room3Button;
+    private Button room4Button;
+    private Button submitButton;
 
     private class Listener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
-            //create explicity intent
-            Intent i = new Intent(getApplicationContext(), ColorActivity.class);//(MainActivity.this, ColorActivity.class.getSimpleName());
-            startActivityForResult(i,0);
+            switch (view.getId()) {
+                case R.id.room1Button:
+                    Intent colorIntent = new Intent(getApplicationContext(), ColorActivity.class);
+                    startActivityForResult(colorIntent,0);
+                    break;
+                case R.id.room2Button:
+                    Intent sizeIntent = new Intent(getApplicationContext(), SizeActivity.class);
+                    startActivityForResult(sizeIntent,0);
+                    break;
+                case R.id.room3Button:
+                    Intent nameIntent = new Intent(getApplicationContext(), NameActivity.class);
+                    startActivityForResult(nameIntent,0);
+                    break;
+                case R.id.room4Button:
+                    Intent imageIntent = new Intent(getApplicationContext(), ImageActivity.class);
+                    startActivityForResult(imageIntent,0);
+                    break;
+                case R.id.submitButton:
+                    break;
+                    default:
+                        break;
+            }
         }
     }
 
@@ -27,13 +50,18 @@ public class MainActivity extends AppCompatActivity {
 
         //connect UI elements
         room1Button = findViewById(R.id.room1Button);
+        room2Button = findViewById(R.id.room1Button);
+        room3Button = findViewById(R.id.room1Button);
+        room4Button = findViewById(R.id.room1Button);
+        submitButton = findViewById(R.id.submitButton);
 
         //set listeners
         Listener listener = new Listener();
         room1Button.setOnClickListener(listener);
+        room2Button.setOnClickListener(listener);
+        room3Button.setOnClickListener(listener);
+        room4Button.setOnClickListener(listener);
+        submitButton.setOnClickListener(listener);
 
-//        Intent i = new Intent(this, ColorActivity.class);
-////        startActivityForResult(i,0);
-//        startActivity(i);
     }
 }
