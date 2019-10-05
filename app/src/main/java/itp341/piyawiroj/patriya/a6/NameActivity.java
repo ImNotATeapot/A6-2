5e9e69238c1390471a6fd7ab84ee0a0f35796f2f
Pearl <imnotateapot@gmail.com>
2019-10-04 23:13:09 -0700

    Size and Name Activities

--- app/src/main/java/itp341/piyawiroj/patriya/a6/NameActivity.java
+++ app/src/main/java/itp341/piyawiroj/patriya/a6/NameActivity.java
@@ -1,4 +1,39 @@
 package itp341.piyawiroj.patriya.a6;
 
-public class NameActivity {
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NameActivity extends AppCompatActivity {
+
+    public EditText editText;
+    public Button saveChangesButton;
+    public String userAnswer = "";
+
+    private static final String TAG = NameActivity.class.getSimpleName();
+
+    private class SaveChangesListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            userAnswer = editText.getText().toString();
+            Log.d(TAG, "User chose " + userAnswer);
+            getIntent().putExtra(MainActivity.EXTRA_USER_ANSWER, userAnswer);
+            setResult(RESULT_OK, getIntent());
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name);
+
+        saveChangesButton = findViewById(R.id.setNameChangesButton);
+        saveChangesButton.setOnClickListener(new SaveChangesListener());
+        editText = findViewById(R.id.nameEditText);
+    }
 }
